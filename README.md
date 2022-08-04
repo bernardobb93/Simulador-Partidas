20cc10c702198c36755cf49414f71791bff22777
bernardobb <bernardobb.eletronica@gmail.com>
2022-08-04 12:04:11 -0300

    teste firebase

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ App Android Nativo de Simulação de Partidas. Este repositório foi organizado 
 ## 2. Componentes, Layouts e UI/UX Em Apps Android 
  - [release/componentes-layouts-ui-ux-em-apps-android](https://github.com/bernardobb93/Simulador-Partidas/tree/release/componentes-layouts-ui-ux-em-apps-android)
 
+## 3. Java e Kotlin Juntos!? Construindo Apps Android 
+ - [release/java-e-kotlin-juntos-construindo-apps-android](https://github.com/bernardobb93/Simulador-Partidas/tree/release/java-e-kotlin-juntos-construindo-apps-android)
+
--- app/src/main/java/com/example/simulator/ui/MainActivity.java
+++ app/src/main/java/com/example/simulator/ui/MainActivity.java
@@ -15,6 +15,7 @@ import com.example.simulator.domain.Match;
 import com.example.simulator.ui.adapter.MatchesAdapter;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MatchesApi matchesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter= new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromApi();
     }
 
